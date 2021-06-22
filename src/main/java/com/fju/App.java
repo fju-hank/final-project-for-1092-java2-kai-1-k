2dbcdacfaf637a84cfeee41f260af41e3b4c8405
kai-1-k <72089746+kai-1-k@users.noreply.github.com>
2021-06-22 18:20:51 +0800

    vs 4.0

--- src/main/java/com/fju/App.java
+++ src/main/java/com/fju/App.java
@@ -2,17 +2,44 @@ package com.fju;
 
 import javax.swing.*;
 
-public class App {
-    private JPanel panel1;
-    private JTextArea textArea1;
+public class App extends JFrame{
+    private JPanel App;
+    private JTextArea order;
     private JButton hotelButton;
     private JButton trainButton;
     private JButton shipButton;
     private JButton airplaneButton;
     private JButton noButton;
     private JButton yesButtom;
+    private JPanel Choice;
+    private JPanel YN;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    public App (String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(App);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame jFrame = new App("My order app");
+        jFrame.setVisible(true);
+        JButton yes = new JButton();
+        JButton no = new JButton();
+        JButton airplane = new JButton();
+        JButton train = new JButton();
+        JButton ship = new JButton();
+        JButton hotel = new JButton();
+        yes.setVisible(true);
+        no.setVisible(true);
+        airplane.setVisible(true);
+        train.setVisible(true);
+        ship.setVisible(true);
+        hotel.setVisible(true);
     }
 }
