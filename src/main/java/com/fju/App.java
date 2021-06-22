a8a5087f7b961733a6af19fd93f91321e91467a3
kai-1-k <72089746+kai-1-k@users.noreply.github.com>
2021-06-22 18:01:02 +0800

    vs 3.0

---
+++ src/main/java/com/fju/App.java
@@ -0,0 +1,18 @@
+package com.fju;
+
+import javax.swing.*;
+
+public class App {
+    private JPanel panel1;
+    private JTextArea textArea1;
+    private JButton hotelButton;
+    private JButton trainButton;
+    private JButton shipButton;
+    private JButton airplaneButton;
+    private JButton noButton;
+    private JButton yesButtom;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
